11890c71c35e00742c8d2382caf6e7c4ed039b48
Yuriy Demchenko <deuriy@yandex.ru>
2021-11-01 00:32:55 +0200

    Header and mobile navigation layout

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,37 @@
-// import { Example } from "./modules/example.js";
+let menuHamburger = document.querySelector('.MenuHamburger');
+let mobileNavigation = document.querySelector('.MobileNavigation');
+let mobileMainMenu = document.querySelector('.MobileNavigation .MainMenu');
+let closeMobileNavigation = document.querySelector('.MobileNavigation_closeBtn');
 
-// new Example();
+function slideToggle(elem) {
+  if (elem.offsetHeight < elem.scrollHeight) {
+    elem.style.maxHeight = `${elem.scrollHeight}px`;
+  } else {
+    elem.style.maxHeight = '';
+  }
+}
+
+menuHamburger.addEventListener('click', function (e) {
+  menuHamburger.classList.toggle('MenuHamburger-active');
+  mobileNavigation.classList.toggle('MobileNavigation-opened');
+
+  document.body.style.overflow = document.documentElement.clientWidth < 768 ? 'hidden' : '';
+
+  e.preventDefault();
+});
+
+// document.addEventListener('click', function (e) {
+//   if (document.documentElement.clientWidth > 767) {
+//     if (!mobileNavigation.contains(e.target) && !menuHamburger.contains(e.target)) {
+//       menuHamburger.classList.remove('MenuHamburger-active');
+//       mobileNavigation.classList.remove('MobileNavigation-opened');
+//     }
+//   }
+// });
+
+closeMobileNavigation.addEventListener('click', function (e) {
+  menuHamburger.classList.remove('MenuHamburger-active');
+  mobileNavigation.classList.remove('MobileNavigation-opened');
+  document.body.style.overflow = '';
+  e.preventDefault();
+});
